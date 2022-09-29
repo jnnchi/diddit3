656bde0773621acd297ac34bb50a7f6792cfdf5a
jnnchi <pl247091@ahschool.com>
2022-09-29 22:17:14 +0000

    Code Synced by DhiWise

---
+++ lib/presentation/completed_screen/completed_screen.dart
@@ -0,0 +1,354 @@
+import '../completed_screen/widgets/completed_item_widget.dart';
+import 'controller/completed_controller.dart';
+import 'models/completed_item_model.dart';
+import 'package:diddit_final/core/app_export.dart';
+import 'package:diddit_final/widgets/custom_button.dart';
+import 'package:diddit_final/widgets/custom_icon_button.dart';
+import 'package:flutter/material.dart';
+
+class CompletedScreen extends GetWidget<CompletedController> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstant.purple50,
+        body: Container(
+          width: size.width,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: size.width,
+                    child: Padding(
+                      padding: getPadding(
+                        left: 33,
+                        right: 40,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: getPadding(
+                              top: 1,
+                              bottom: 1,
+                            ),
+                            child: CommonImageView(
+                              svgPath: ImageConstant.imgHome,
+                              height: getVerticalSize(
+                                19.00,
+                              ),
+                              width: getHorizontalSize(
+                                25.00,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: getPadding(
+                              top: 1,
+                            ),
+                            child: CommonImageView(
+                              svgPath: ImageConstant.imgCheckmark,
+                              height: getSize(
+                                20.00,
+                              ),
+                              width: getSize(
+                                20.00,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: getPadding(
+                              bottom: 1,
+                            ),
+                            child: CommonImageView(
+                              svgPath: ImageConstant.imgSettings,
+                              height: getVerticalSize(
+                                20.00,
+                              ),
+                              width: getHorizontalSize(
+                                25.00,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(
+                    left: 25,
+                    top: 2,
+                    right: 25,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Padding(
+                        padding: getPadding(
+                          bottom: 1,
+                        ),
+                        child: Text(
+                          "lbl_home".tr,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: AppStyle.txtPoppinsRegular14,
+                        ),
+                      ),
+                      Padding(
+                        padding: getPadding(
+                          top: 1,
+                        ),
+                        child: Text(
+                          "lbl_completed".tr,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: AppStyle.txtPoppinsRegular14,
+                        ),
+                      ),
+                      Padding(
+                        padding: getPadding(
+                          top: 1,
+                          bottom: 1,
+                        ),
+                        child: Text(
+                          "lbl_account".tr,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: AppStyle.txtPoppinsRegular14,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: getHorizontalSize(
+                    159.00,
+                  ),
+                  margin: getMargin(
+                    left: 25,
+                    top: 30,
+                    right: 25,
+                  ),
+                  child: Text(
+                    "msg_completed_assig".tr,
+                    maxLines: null,
+                    textAlign: TextAlign.center,
+                    style: AppStyle.txtWorkSansRomanSemiBold25,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 25,
+                      top: 25,
+                      right: 25,
+                    ),
+                    child: Text(
+                      "lbl_yesterday".tr,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: AppStyle.txtInterBold12,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: getPadding(
+                    left: 25,
+                    top: 6,
+                    right: 25,
+                  ),
+                  child: Obx(
+                    () => ListView.builder(
+                      physics: BouncingScrollPhysics(),
+                      shrinkWrap: true,
+                      itemCount: controller
+                          .completedModelObj.value.completedItemList.length,
+                      itemBuilder: (context, index) {
+                        CompletedItemModel model = controller
+                            .completedModelObj.value.completedItemList[index];
+                        return CompletedItemWidget(
+                          model,
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 25,
+                      top: 26,
+                      right: 25,
+                    ),
+                    child: Text(
+                      "lbl_today".tr,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: AppStyle.txtInterBold12,
+                    ),
+                  ),
+                ),
+                Container(
+                  width: double.infinity,
+                  margin: getMargin(
+                    left: 25,
+                    top: 2,
+                    right: 25,
+                  ),
+                  decoration: AppDecoration.outlineDeeppurpleA100891.copyWith(
+                    borderRadius: BorderRadiusStyle.roundedBorder10,
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: getPadding(
+                            left: 14,
+                            top: 10,
+                            right: 14,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Padding(
+                                padding: getPadding(
+                                  top: 1,
+                                  bottom: 3,
+                                ),
+                                child: Text(
+                                  "lbl_class_name".tr,
+                                  overflow: TextOverflow.ellipsis,
+                                  textAlign: TextAlign.left,
+                                  style: AppStyle.txtInterLight10RedA200,
+                                ),
+                              ),
+                              CustomIconButton(
+                                height: 15,
+                                width: 15,
+                                child: CommonImageView(
+                                  svgPath: ImageConstant.imgCheckmark15X15,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: getPadding(
+                          left: 14,
+                          top: 5,
+                          right: 14,
+                        ),
+                        child: Text(
+                          "lbl_assignment_name".tr,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.left,
+                          style: AppStyle.txtInterRegular12,
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Padding(
+                          padding: getPadding(
+                            left: 14,
+                            top: 9,
+                            right: 14,
+                            bottom: 10,
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              CustomButton(
+                                width: 47,
+                                text: "lbl_okay".tr,
+                              ),
+                              Padding(
+                                padding: getPadding(
+                                  top: 3,
+                                  bottom: 2,
+                                ),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Padding(
+                                      padding: getPadding(
+                                        top: 1,
+                                      ),
+                                      child: CommonImageView(
+                                        svgPath: ImageConstant.imgPlay,
+                                        height: getSize(
+                                          12.00,
+                                        ),
+                                        width: getSize(
+                                          12.00,
+                                        ),
+                                      ),
+                                    ),
+                                    Padding(
+                                      padding: getPadding(
+                                        left: 3,
+                                        bottom: 1,
+                                      ),
+                                      child: Text(
+                                        "lbl_11_59pm".tr,
+                                        overflow: TextOverflow.ellipsis,
+                                        textAlign: TextAlign.left,
+                                        style: AppStyle.txtInterRegular12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: getPadding(
+                      left: 27,
+                      top: 19,
+                      right: 27,
+                      bottom: 20,
+                    ),
+                    child: Text(
+                      "lbl_tomorrow".tr,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: AppStyle.txtInterBold12,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/presentation/sign_in_screen/sign_in_screen.dart
@@ -0,0 +1,165 @@
+import 'controller/sign_in_controller.dart';
+import 'package:diddit_final/core/app_export.dart';
+import 'package:diddit_final/core/utils/validation_functions.dart';
+import 'package:diddit_final/widgets/custom_button.dart';
+import 'package:diddit_final/widgets/custom_text_form_field.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends GetWidget<SignInController> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: ColorConstant.whiteA700,
+        body: Container(
+          width: size.width,
+          child: SingleChildScrollView(
+            child: Form(
+              key: _formKey,
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Padding(
+                    padding: getPadding(
+                      left: 26,
+                      top: 131,
+                      right: 26,
+                    ),
+                    child: Text(
+                      "lbl_welcome_back".tr,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: AppStyle.txtWorkSansRomanRegular20,
+                    ),
+                  ),
+                  Padding(
+                    padding: getPadding(
+                      left: 26,
+                      top: 25,
+                      right: 24,
+                    ),
+                    child: CommonImageView(
+                      imagePath: ImageConstant.imgUndrawsignin,
+                      height: getVerticalSize(
+                        200.00,
+                      ),
+                      width: getHorizontalSize(
+                        324.00,
+                      ),
+                    ),
+                  ),
+                  CustomTextFormField(
+                    width: 324,
+                    focusNode: FocusNode(),
+                    controller: controller.textFieldController,
+                    hintText: "msg_enter_your_emai".tr,
+                    margin: getMargin(
+                      left: 26,
+                      top: 26,
+                      right: 25,
+                    ),
+                    validator: (value) {
+                      if (value == null ||
+                          (!isValidEmail(value, isRequired: true))) {
+                        return "Please enter valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                  CustomTextFormField(
+                    width: 324,
+                    focusNode: FocusNode(),
+                    controller: controller.textFieldOneController,
+                    hintText: "msg_enter_your_pass".tr,
+                    margin: getMargin(
+                      left: 26,
+                      top: 25,
+                      right: 25,
+                    ),
+                    textInputAction: TextInputAction.done,
+                    validator: (value) {
+                      if (value == null ||
+                          (!isValidPassword(value, isRequired: true))) {
+                        return "Please enter valid password";
+                      }
+                      return null;
+                    },
+                    isObscureText: true,
+                  ),
+                  Padding(
+                    padding: getPadding(
+                      left: 26,
+                      top: 19,
+                      right: 26,
+                    ),
+                    child: Text(
+                      "msg_forgot_password".tr,
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                      style: AppStyle.txtInterRegular12Purple800,
+                    ),
+                  ),
+                  CustomButton(
+                    width: 325,
+                    text: "lbl_sign_in2".tr,
+                    margin: getMargin(
+                      left: 26,
+                      top: 55,
+                      right: 24,
+                    ),
+                    variant: ButtonVariant.FillDeeppurpleA100,
+                    shape: ButtonShape.Square,
+                    padding: ButtonPadding.PaddingAll19,
+                    fontStyle: ButtonFontStyle.WorkSansRomanSemiBold13,
+                  ),
+                  Container(
+                    margin: getMargin(
+                      left: 26,
+                      top: 14,
+                      right: 26,
+                      bottom: 20,
+                    ),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "msg_don_t_have_an_a2".tr,
+                            style: TextStyle(
+                              color: ColorConstant.black900,
+                              fontSize: getFontSize(
+                                12,
+                              ),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          TextSpan(
+                            text: "lbl_sign_up".tr,
+                            style: TextStyle(
+                              color: ColorConstant.purple800,
+                              fontSize: getFontSize(
+                                12,
+                              ),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ],
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
